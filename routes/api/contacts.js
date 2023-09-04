ab8a1c4cfaac4d7f0f831713c32ab41436266a1d
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-09-04 15:15:33 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
-const { listContacts, getContactById } = require('../../models/contacts');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require('../../models/contacts');
 
 const router = express.Router();
 
@@ -31,22 +37,41 @@ router.get('/:contactId', async (req, res, next) => {
   }
 });
 
-// router.post('/', createContact);
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const result = await addContact({ name, email, phone });
 
-// router.delete('/:contactId', deleteContact);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
-// router.put('/:contactId', changeContact);
+router.delete('/:contactId', async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await removeContact(contactId);
 
-// router.post('/', async (req, res, next) => {
-//   res.json({ message: 'template message' });
-// });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
-// router.delete('/:id', async (req, res, next) => {
-//   res.json({ message: 'template message' });
-// });
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { name, email, phone } = req.body;
+    const result = await updateContact(contactId, { name, email, phone });
 
-// router.put('/:id', async (req, res, next) => {
-//   res.json({ message: 'template message' });
-// });
+    if (!result) {
+      res.status(200).json(result);
+      res.status(404).json({ message: `Contact by ID ${contactId}: not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 module.exports = router;
