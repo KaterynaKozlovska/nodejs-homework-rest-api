a47d8803a38580eb51a9d6f855819d789eaab0cf
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-09-11 14:37:04 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,16 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody } = require('../../middlewares');
-
-const { contactsAddSchema, contactsUpdateSchema } = require('../../schemas/joiShema');
-
 const router = express.Router();
 
 router.get('/', ctrl.getAllContacts);
 
 router.get('/:contactId', ctrl.getById);
 
-router.post('/', validateBody(contactsAddSchema), ctrl.add);
+router.post('/', ctrl.add);
 
 router.delete('/:contactId', ctrl.removeById);
 
-router.put('/:contactId', validateBody(contactsUpdateSchema), ctrl.updateById);
+router.put('/:contactId', ctrl.updateById);
 
 module.exports = router;
