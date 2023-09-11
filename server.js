d191ea58088736939f2dd02bf362fd986037498e
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-09-11 14:42:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_HOST =
-  'mongodb+srv://kkozlovska:GTzne6tDFnUIy9kf@cluster0.nnkvr01.mongodb.net/db-contacts?retryWrites=true&w=majority';
-
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
     app.listen(3000, () => {
       console.log('Database connection successful. Server running. Use our API on port: 3000');
