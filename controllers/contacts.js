6ea03794516968cb0e04ee82eab4cb8e5a37c7db
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-09-11 14:33:58 +0300

    Revert "add controllers"
    This reverts commit 6f452bdc2dd4e08e935afd7e829c124d83699bd7.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -7,6 +7,7 @@ const {
 } = require('../models/contacts');
 
 const { HttpError, ctrlWrapper } = require('../helpers');
+const { contactsAddSchema, contactsUpdateSchema } = require('../schemas/joiShema');
 
 const getAllContacts = async (req, res) => {
   const result = await listContacts();
@@ -23,6 +24,10 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
+  const { error } = contactsAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, 'Missing required name field');
+  }
   const result = await addContact(req.body);
   res.status(201).json(result);
 };
@@ -37,6 +42,10 @@ const removeById = async (req, res) => {
 };
 
 const updateById = async (req, res) => {
+  const { error } = contactsUpdateSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, 'Missing fields');
+  }
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
   if (!result) {
