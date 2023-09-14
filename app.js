029e4dc33cdd32e26dbbef8604bb27aa7cfa46fa
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-09-14 17:14:10 +0300

    create auth

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import contactsRouter from './routes/api/contacts.js';
-
+import authRouter from './routes/api/auth-router.js';
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
-
+app.use('/api/auth', authRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
